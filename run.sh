ccfdea9f41dbd6f936e75dcab38500a9112d5704
AntonGorokhov <tonygorokhov@gmail.com>
2021-07-24 22:08:08 +0300

    added run.sh for using

--- run.sh
+++ run.sh
@@ -1,1 +1,7 @@
-
+g++ helper.cpp -o helper -O2 -std=c++17
+echo "Enter a line you want to endode : "
+./helper > input.txt
+./encode.sh
+./analysis.sh
+echo "If you want to see the encoding, type 'cat output.txt'"
+echo "If you want to decode the message, type './decode.sh'" 
